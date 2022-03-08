62ae412acd7c8ba17bb400c0a5f1655ab998cd8f
Hoangthanhphu <101129439+Hoangthanhphu@users.noreply.github.com>
2022-03-08 11:42:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+ const nameGirl = '8-3 vui vẻ';
+const giftUrl = 'index1.html';
+const eventName = 'Chúc em';
+const titleCard = 'Tặng các em';
+const contentCard = 'Chúc em ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong ngày hôm nay và những ngày mai sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '10.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
